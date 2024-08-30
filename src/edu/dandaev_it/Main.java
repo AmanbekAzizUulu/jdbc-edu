cd05bb4268837511ea1e73d889d24d59a7c6465c
Amanbek <amanbekazizuulu@gmail.com>
2024-08-31 04:16:47 +0600

    safety selection flights between two date-times via preparedStatement objects executeQuery methods

--- src/edu/dandaev_it/Main.java
+++ src/edu/dandaev_it/Main.java
@@ -2,7 +2,10 @@ package edu.dandaev_it;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,17 +14,47 @@ import edu.dandaev_it.util.ConnectionManager;
 public class Main {
 
 	public static void main(String[] args) {
-		Long flightId = 2L;
+		LocalDateTime start = LocalDate.of(2020, 06, 1).atStartOfDay();
+		LocalDateTime end = LocalDateTime.now();
 
 		try (Connection connection = ConnectionManager.open()) {
 			System.out.println("\033[0;32mConnection established successfully!\033[0m");
 
-			List<Long> ticketsByFlightId = getTicketsByFlightId(flightId);
-			System.out.println("tickets with flight_id = " + ticketsByFlightId);
+			List<Long> flightsBetween = getFlightsBetween(start, end);
+			System.out.println("flights between " + start + " and " + end + " : " + flightsBetween);
 
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+	}
+
+	private static List<Long> getFlightsBetween(LocalDateTime start, LocalDateTime end) throws SQLException {
+		List<Long> selectedId = new ArrayList<Long>();
+		String select_flights_between = """
+						select
+							id
+						from
+							flight
+						where
+							departure_date between ? and ?;
+
+				""";
+
+		try (Connection connection = ConnectionManager.open();
+				PreparedStatement preparedStatement = connection.prepareStatement(select_flights_between)) {
+			System.out.println(preparedStatement);
+		    preparedStatement.setTimestamp(1, java.sql.Timestamp.valueOf(start));
+			System.out.println(preparedStatement);
+			preparedStatement.setTimestamp(2, java.sql.Timestamp.valueOf(end));
+			System.out.println(preparedStatement);
+
+			ResultSet resultSet = preparedStatement.executeQuery();
+
+			while (resultSet.next()) {
+				selectedId.add(resultSet.getLong(1));
+			}
+		}
+		return selectedId;
 	}
 
 	private static List<Long> getTicketsByFlightId(Long flightId) throws SQLException {
