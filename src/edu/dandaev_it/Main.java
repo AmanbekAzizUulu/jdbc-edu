40cdb34977b503813190aba89c9fb23e0f9d31cb
Amanbek <amanbekazizuulu@gmail.com>
2024-09-02 22:04:50 +0600

    not using transactions, see explanation in README.md file

--- src/edu/dandaev_it/Main.java
+++ src/edu/dandaev_it/Main.java
@@ -19,22 +19,31 @@ public class Main {
 				""";
 		Long flight_id = 3L;
 
+		String delete_referenced_ticket = """
+				delete
+				from
+					ticket
+				where
+					flight_id = ?
+				""";
+
 		String select_all_from_flight_table = """
 				select
 					id
 				from
 					flight
 				""";
+
 		try (Connection connection = ConnectionManager.open();
 				PreparedStatement preparedStatement_1 = connection.prepareStatement(delete_flight_by_id);
-				PreparedStatement preparedStatement_2 =  connection.prepareStatement(select_all_from_flight_table)) {
+				PreparedStatement preparedStatement_2 = connection.prepareStatement(delete_referenced_ticket);
+				PreparedStatement preparedStatement_3=  connection.prepareStatement(select_all_from_flight_table)) {
 
-					// Cannot delete or update a parent row: a foreign key constraint fails
 					preparedStatement_1.setLong(1, flight_id);
-
+					preparedStatement_2.setLong(1, flight_id);
 
 					preparedStatement_1.executeUpdate();
-					ResultSet resultSet = preparedStatement_2.executeQuery();
+					ResultSet resultSet = preparedStatement_3.executeQuery();
 					while (resultSet.next()) {
 						System.out.println("id = " + resultSet.getInt(1));
 					}
